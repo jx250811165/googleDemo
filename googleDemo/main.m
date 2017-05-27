3b0eebb99c5e8614ad34ea4878f067eeb07b284e
166 <apple@urnot166-demac-mini.local>
2017-05-27 12:08:33 +0800

    Initial Commit

---
+++ googleDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  googleDemo
+//
+//  Created by 166 on 2017/5/27.
+//  Copyright © 2017年 166. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
